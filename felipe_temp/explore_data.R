496ea9fb44b8f46a61746132cd0be39b26322c85
Felipe Marin <fmarin@astro.swin.edu.au>
2016-06-25 19:01:12 +1000

    added categories for breeds

--- felipe_temp/explore_data.R
+++ felipe_temp/explore_data.R
@@ -52,11 +52,61 @@ plot.outday <- plot.outday + geom_bar(aes(fill=factor(OutcomeType)),position='fi
 plot.outday <- plot.outday + labs(title='Outcome vs Day',x='Day of the week',y='Normalised count')
 plot.outday
 
+#add pet's race into the mix
+# remove special characters (replace with ' ')
+PetBreed <- data.frame(PetBreed=tolower(gsub("[^[:alnum:] ]", " ",trainPrep.df$Breed)))
+#eliminate mix
+trainPrep.df$PetBreed <- as.character(sub("(.*?) mix.*", "\\1",PetBreed$PetBreed))
 
+#create mapping: cats
+cat.type <- data.frame(table(as.character(trainPrep.df[trainPrep.df$AnimalType=='Cat',]$PetBreed)))
+names(cat.type) <- c('Type','Freq')
+cat.type %<>% arrange(desc(Freq)) %>%
+                mutate(cumsumFreq=cumsum(Freq)/sum(Freq)) 
+#take the first 9 classes - 10th is 'other'
+cat.main.types <- cat.type[1:9,] %>% arrange(Freq) %>% select(Type)
+cat.type$Category <- rep("",nrow(cat.type))
+cat.type$Category[1:9] <- as.character(cat.type$Type[1:9])
 
+for (i in 10:length(cat.type$Type)){
+  cat.type$Category[i] = 'other cat'
+  for(j in 1:9){
+    if(grepl(cat.main.types$Type[j],cat.type$Type[i])==TRUE){
+      cat.type$Category[i] = as.character(cat.main.types$Type[j])
+      break
+    }
+  }
+}
 
+  
+#create mapping: dogs
+dog.type <- data.frame(table(as.character(trainPrep.df[trainPrep.df$AnimalType=='Dog',]$PetBreed)))
+names(dog.type) <- c('Type','Freq')
+dog.type %<>% arrange(desc(Freq)) %>%
+    mutate(cumsumFreq=cumsum(Freq)/sum(Freq)) 
+#take the first 14 classes - 15th is 'other'
+dog.main.types <- data.frame(Type=c('pit bull','chihuahua','german shepperd','australian cattle','retriever',
+                      'dachshund','poodle','terrier','schnauzer','boxer','shepperd','shih tzu',
+                      'beagle','husky')) 
+  
+for (i in 1:length(dog.type$Type)){
+  dog.type$Category[i] = 'other dog'
+    for(j in 1:14){
+      if(grepl(dog.main.types$Type[j],dog.type$Type[i])==TRUE){
+        dog.type$Category[i] = as.character(dog.main.types$Type[j])
+        break
+      }
+    }
+}
 
+#create new table
+pet.MainBreed <- rbind(dog.type,cat.type) %>% select(PetBreed=Type,PetBreedCat=Category)
+#join
+trainPrep.df %<>% inner_join(pet.MainBreed)
 
-
-
-
+#explore data
+#outcome vs Main breed category
+plot.breedmain <- ggplot(data=trainPrep.df,aes(x=factor(PetBreedCat)))
+plot.breedmain <- plot.breedmain + geom_bar(aes(fill=factor(OutcomeType)),position='fill')
+plot.breedmain <- plot.breedmain + labs(title='Outcome vs Day',x='Breed Category',y='Normalised count')
+plot.breedmain
